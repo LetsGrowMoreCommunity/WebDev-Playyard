6991679fbdee8114ea62a496e3043ea2daffd640
anushruti11 <anushruti1132001@gmail.com>
2021-08-03 11:00:36 +0530

    location update

---
+++ Projects/Calculator/script/calculator.js
@@ -0,0 +1,13 @@
+$(".numbers").on('click','li',function(e){
+    e.preventDefault();
+    $v = $(this).html(),
+    $r = $('.result');
+    if($r.text()==0){$r.empty()}
+    if($v==="=" ) {
+      r = eval($r.text());
+      $r.empty().append(r);
+      return true;}
+    if($v==="c" ) {$r.empty().append(0);return true;}
+    $r.append($v)
+  }) 
+  
