b8c2df1078dcfb6cb5a4f0096846c1e7a90a5396
Ruhee Jain <59756474+Ruheej1@users.noreply.github.com>
2021-08-08 22:45:13 +0530

    Update index.js

--- Projects/The Dice Game/index.js
+++ Projects/The Dice Game/index.js
@@ -1,7 +1,10 @@
+//generates a random number from 1 to 6
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
+//selects a random image 
 var randomDiceImage = "dice" + randomNumber1 + ".png";
 
+//sets the source of img1 according to random img chosen
 var randomImageSource = "Assets/media/" + randomDiceImage;
 
 var image1 = document.querySelectorAll("img")[0];
@@ -10,6 +13,7 @@ image1.setAttribute("src", randomImageSource);
 
 var randomNumber2 = Math.floor(Math.random() * 6)+1;
 
+//sets the source of img2 according to random img chosen
 var randomImageSource2 = "Assets/media/dice" + randomNumber2 + ".png";
 
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
